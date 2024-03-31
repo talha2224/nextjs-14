2969ede1cf8719b126419b7848f386ed48f52458
Talha <talhahaider074@gmail.com>
2024-03-31 18:00:29 +0500

    auth added

--- app/firebase.ts
+++ app/firebase.ts
@@ -5,8 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 export const createFirebaseApp = () => {
   const clientCredentials = {
     apiKey: "AIzaSyCpcH84pZMsopqc6jSsWaUDiiMtK4E0Uwc",
-    // authDomain: "kayyo-2378d.firebaseapp.com",
-    authDomail:"next-app-14.netlify.app",
+    authDomain: "kayyo-2378d.firebaseapp.com",
     projectId: "kayyo-2378d",
     storageBucket: "kayyo-2378d.appspot.com",
     messagingSenderId: "438239183645",
